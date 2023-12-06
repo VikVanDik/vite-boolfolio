2736248060600721fe9be69dcc5ea6d912c9c8b9
VikVanDik <95vlc17@gmail.com>
2023-12-06 17:46:02 +0100

    [add] Loader component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 import axios from 'axios';
 import {store} from './data/store';
 import ProjectCard from './components/ProjectCard.vue';
+import Loader from './components/Loader.vue';
 
 export default {
 
@@ -10,18 +11,21 @@ export default {
 
   data(){
     return {
-      message: "Hello VUEEEEEEEEEEEEEEE!"
+      message: "Lista progetti",
+      isLoaded: false
     }
   },
 
   components: {
-    ProjectCard
+    ProjectCard,
+    Loader
   },
 
   methods:{
     getApi(){
       axios.get(store.apiUrl + 'projects')
         .then(results =>{
+          this.isLoaded = true;
           store.projects = results.data.data;
         })
     }
@@ -36,10 +40,17 @@ export default {
 </script>
 
 <template>
- <h1>{{ message }}</h1>
- <ProjectCard></ProjectCard>
+ <Loader v-if="!isLoaded"/>
+ <div v-else>
+   <h1>{{ message }}</h1>
+   <ProjectCard></ProjectCard>
+ </div>
 </template>
 
 <style lang="scss">
-
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 </style>
