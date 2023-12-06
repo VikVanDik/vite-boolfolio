73b12a694e04a7c818b692f5958dee6ce782619d
VikVanDik <95vlc17@gmail.com>
2023-12-06 16:29:29 +0100

    [add] axios call

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <script>
 
+import axios from 'axios';
+import store from './data/store';
+
 export default {
+
   name : "App",
+
   data(){
     return {
       message: "Hello VUEEEEEEEEEEEEEEE!"
     }
+  },
+
+  methods:{
+    getApi(){
+      axios.get(store.apiUrl + 'projects')
+        .then(results =>{
+          console.log(results.data);
+        })
+    }
+  },
+
+  mounted(){
+    this.getApi();
   }
 }
+
 
 </script>
 
